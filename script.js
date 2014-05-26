4d6f9866c10e99d83d8c948adba9b31be1e80399
jkbits1 <jkbits1@users.noreply.github.com>
2014-05-27 00:44:35 +0100

    Changed formatDate variables to be local rather than global

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 // TV Schedule example
 
 function formatDate(start, end) {
-  start_date = new Date(start);
-  end_date = new Date(end);
 
-  day = start_date.getDate();
-  month = start_date.getMonth() + 1; // the returned months are 0-11
-  year = start_date.getFullYear();
+    var start_date = new Date(start);
+    var end_date = new Date(end);
 
-  start_hour = start_date.getHours();
-  start_mins = start_date.getMinutes();
+    var day = start_date.getDate();
+    var month = start_date.getMonth() + 1; // the returned months are 0-11
+    var year = start_date.getFullYear();
 
-  end_hour = end_date.getHours();
-  end_mins = end_date.getMinutes();
+    var start_hour = start_date.getHours();
+    var start_mins = start_date.getMinutes();
 
-  date = day + "/" + month + "/" + year + " ";
-  
-  // add leading 0 and return last two characters to make sure we use 00:00 format
-  date +=  ("0"+start_hour).slice(-2) + ":" + ("0"+start_mins).slice(-2) + " - " +
-           ('0' + end_hour).slice(-2) + ":" +  ( "0" + end_mins).slice(-2); 
-  return date;
+    var end_hour = end_date.getHours();
+    var end_mins = end_date.getMinutes();
+
+    var date = day + "/" + month + "/" + year + " ";
+
+    // add leading 0 and return last two characters to make sure we use 00:00 format
+    date +=  ("0"+start_hour).slice(-2) + ":" + ("0"+start_mins).slice(-2) + " - " +
+        ('0' + end_hour).slice(-2) + ":" +  ( "0" + end_mins).slice(-2);
+
+    return date;
 }
